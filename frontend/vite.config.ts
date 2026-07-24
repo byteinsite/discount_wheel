0d9ca0e19a9b6a17852a4e54e311955f407ecd9d
Byteinsite <byteinsite@yandex.ru>
2026-07-25 02:55:01 +0300

    fix: allow railway host in vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'discountwheel-copy-production.up.railway.app' // Ваш домен на Railway
+    ]
+  },
   server: {
     host: true,
     port: 5173,
