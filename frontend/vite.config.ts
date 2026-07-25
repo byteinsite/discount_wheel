df55e2df0b213827531e5c0f13b912e411589a85
Byteinsite <byteinsite@yandex.ru>
2026-07-25 03:15:33 +0300

    fix: edit vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    allowedHosts: [
-      'discountwheel-copy-production.up.railway.app' // Ваш домен на Railway
-    ]
-  },
   server: {
     host: true,
     port: 5173,
@@ -19,4 +14,17 @@ export default defineConfig({
       "/health": "http://localhost:8000",
     },
   },
+  preview: {
+    allowedHosts: 'all', // Убирает ошибку "Blocked request"
+    host: true,
+    port: Number(process.env.PORT) || 4173, // Передаем порт от Railway
+    proxy: {
+      // Подставляем URL вашего FastAPI бэкенда на Railway
+      "/auth": process.env.VITE_API_URL || "http://localhost:8000",
+      "/me": process.env.VITE_API_URL || "http://localhost:8000",
+      "/spin": process.env.VITE_API_URL || "http://localhost:8000",
+      "/result": process.env.VITE_API_URL || "http://localhost:8000",
+      "/health": process.env.VITE_API_URL || "http://localhost:8000",
+    }
+  }
 });
